831287fb27c5bc909fcc0b7f471ff8be2123b61a
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-08-31 08:12:36 +0600

    updated

--- NML-02/comparisonBetweenMethods.cpp
+++ NML-02/comparisonBetweenMethods.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 using namespace std;
 
+const double x_true = ((-3 + sqrt(57.0)) / 8.0);
+
 double f(double x) {
     return 4 * x * x + 3 * x - 3;
 }
@@ -14,11 +16,15 @@ int bisection(double a, double b, double tolerance) {
 
     double c;
     int iteration = 0;
-
+    double truePercentRelativeError;
 
     while ((b - a) >= tolerance) {
         c = (a + b) / 2;
         iteration++;
+        truePercentRelativeError = fabs((x_true - c) / x_true) * 100;
+        
+    
+        cout << "c = " << c << ", TPRE = " << truePercentRelativeError <<"%" <<endl;
 
         if (f(c) == 0.0) {
             break; // Exact root found
@@ -28,10 +34,11 @@ int bisection(double a, double b, double tolerance) {
             b = c;
         else
             a = c;
-
+        
+        
     }
 
-    cout << fixed << setprecision(6);
+
     cout << "Approximate root in traditional bisection: " << c <<"  ";
     cout << "iterations: " << iteration << endl;
     return iteration;
@@ -46,11 +53,16 @@ int regularFalsePosition(double a, double b, double tolerance) {
 
     double c;
     int iteration = 0;
+    double truePercentRelativeError;
 
     while ((b - a) >= tolerance) {
         
         c = (a * f(b) - b * f(a)) / (f(b) - f(a));
         iteration++;
+        
+        truePercentRelativeError = fabs((x_true - c) / x_true) * 100;
+    
+        cout << "c = " << c << ", TPRE = " << truePercentRelativeError <<"%" << endl;
 
         if (f(c) == 0.0) {
             break; // Exact root found
@@ -62,10 +74,10 @@ int regularFalsePosition(double a, double b, double tolerance) {
         else
             a = c;
         
-        cout << fixed << setprecision(6);
+    
     }
 
-    cout << fixed << setprecision(6);
+
     cout << "Approximate root in False Position Method: " << c <<"  iterations: " << iteration << endl;
     return iteration;
 }
@@ -73,14 +85,14 @@ int regularFalsePosition(double a, double b, double tolerance) {
 int main() {
     double a = 0;            
     double b = 1;             
-    double tolerance = 1e-6;
+    double tolerance = 1e-4;
     double stepSize = 0.1;
+
+    //cout << fixed << setprecision(6);
 
     int iter1 = bisection(a, b, tolerance);
     int iter2 = regularFalsePosition(a,b,tolerance);
 
-    cout << "Traditional Bisection Iterations: " << iter1 <<endl;
-    cout << "False Position Method Iterations: " <<iter2 << endl;
 
     return 0;
 }

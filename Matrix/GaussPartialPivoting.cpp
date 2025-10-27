3fcba3671fb07d53cf5f23b9af73f8fb645e3fce
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-10-27 11:35:08 +0600

    make it one-based matrix

--- Matrix/GaussPartialPivoting.cpp
+++ Matrix/GaussPartialPivoting.cpp
@@ -1,46 +1,42 @@
 #include <bits/stdc++.h>
-#include<iomanip>
-
+#include <iomanip>
 using namespace std;
 
-double arr[20][20]; // Augmented matrix
-double x[20];       // Solution vector
+double arr[25][25]; // Augmented matrix (1-based)
+double x[25];       // Solution vector (1-based)
 
 void gaussElimination(int n, int m) {
-    
-    int prow = 0, pcol = 0;
 
-    while (pcol < m && prow < n) {
-        
-        // Find pivot row (largest absolute value)
-        
+    int prow = 1;
+    int pcol = 1;
+
+    while (pcol <= m && prow <= n) {
+
+        // Find pivot row
         int pivot = prow;
-        for (int i = prow + 1; i < n; i++) {
+        for (int i = prow + 1; i <= n; i++) {
             if (fabs(arr[i][pcol]) > fabs(arr[pivot][pcol])) {
                 pivot = i;
             }
         }
 
-        // If pivot element is zero, skip this column
-        
+        // If pivot is zero, move to next column
         if (fabs(arr[pivot][pcol]) < 1e-9) {
             pcol++;
             continue;
         }
 
-        // Swap current row with pivot row
-        
+        // Swap pivot row to current position
         if (pivot != prow) {
-            for (int i = 0; i <= m; i++) {
-                swap(arr[pivot][i], arr[prow][i]);
+            for (int j = 1; j <= m + 1; j++) {
+                swap(arr[pivot][j], arr[prow][j]);
             }
         }
 
-        // Eliminate below pivot
-        
-        for (int i = prow + 1; i < n; i++) {
+        // Eliminate rows below pivot
+        for (int i = prow + 1; i <= n; i++) {
             double factor = arr[i][pcol] / arr[prow][pcol];
-            for (int j = pcol; j <= m; j++) {
+            for (int j = pcol; j <= m + 1; j++) {
                 arr[i][j] -= factor * arr[prow][j];
             }
         }
@@ -49,24 +45,31 @@ void gaussElimination(int n, int m) {
         pcol++;
     }
 
-    // Back Substitution
-    
-    for (int i = n - 1; i >= 0; i--) {
-        x[i] = arr[i][m]; // Start with constant term
-        for (int j = i + 1; j < m; j++) {
+    // Back substitution
+    for (int i = n; i >= 1; i--) {
+
+        if (fabs(arr[i][i]) < 1e-9) {
+            cout << "No unique solution exists!" << endl;
+            return;
+        }
+
+        x[i] = arr[i][m + 1];
+
+        for (int j = i + 1; j <= m; j++) {
             x[i] -= arr[i][j] * x[j];
         }
-        x[i] /= arr[i][i]; // Divide by pivot
+
+        x[i] /= arr[i][i];
     }
 }
 
 int main() {
-    int n = 3; // Number of equations
-    int m = 3; // Number of variables
+    int n = 3;
+    int m = 3;
 
-    cout << "Enter augmented matrix (each row: a b c | d):" << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= m; j++) {
+    cout << "Enter augmented matrix (1-based):" << endl;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m + 1; j++) {
             cin >> arr[i][j];
         }
     }
@@ -74,8 +77,9 @@ int main() {
     gaussElimination(n, m);
 
     cout << "\nSolution:\n";
-    for (int i = 0; i < n; i++) {
-        cout << "x[" << i << "] = " << fixed << setprecision(5) << x[i] << endl;
+    for (int i = 1; i <= n; i++) {
+        cout << "x[" << i << "] = " << fixed 
+             << setprecision(5) << x[i] << endl;
     }
 
     return 0;
